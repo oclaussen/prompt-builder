38f8989fee35f4484c6c0c9df627795232407465
Ole Claussen <claussen.ole@gmail.com>
2016-05-12 16:27:52 +0200

    moved code from the dotfile cookbook to here

---
+++ lib/prompt_builder/constants.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+#
+# Copyright 2016, Ole Claussen <claussen.ole@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module PromptBuilder
+  FOREGROUNDS = {
+    enable: {
+      black: 30, red: 31, green: 32, yellow: 33,
+      blue: 34, magenta: 35, cyan: 36, white: 37
+    },
+    disable: 38
+  }.freeze
+
+  BACKGROUNDS = {
+    enable: {
+      black: 40, red: 41, green: 42, yellow: 43,
+      blue: 44, magenta: 45, cyan: 46, white: 47
+    },
+    disable: 48
+  }.freeze
+
+  ATTRIBUTES = {
+    enable: { bold: 1, underline: 4, blink: 5, reverse: 7, concealed: 8 },
+    disable: { bold: 22, underline: 24, blink: 25, reverse: 27, concealed: 28 }
+  }.freeze
+end
---
+++ lib/prompt_builder/prompt.rb
@@ -0,0 +1,78 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+#
+# Copyright 2016, Ole Claussen <claussen.ole@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'prompt_builder/segment'
+
+module PromptBuilder
+  class Prompt
+    def self.provide_escape_sequence(name, text = nil, &blk)
+      if text.nil? && block_given?
+        define_method name do |*args, **kwargs|
+          args << kwargs unless kwargs.empty?
+          Segment.new(instance_exec(*args, &blk))
+        end
+      else
+        define_method name do
+          Segment.new(text)
+        end
+      end
+    end
+
+    def initialize(*args)
+      @segments = []
+      segment(args) unless args.empty?
+    end
+
+    def segment(*args, **kwargs)
+      @segments << Segment.new(*args, **kwargs, noprint: method(:noprint))
+    end
+
+    def to_s
+      @segments.map(&:to_s).join
+    end
+
+    def noprint(_text)
+      raise 'No noprint escape sequence defined for prompt.'
+    end
+
+    provide_escape_sequence(:literal) { |text| text }
+    provide_escape_sequence(:clear) { noprint '\033[0m' }
+
+    def colored(*args, **kwargs)
+      Segment.new(*args, **kwargs, noprint: method(:noprint))
+    end
+
+    FOREGROUNDS[:enable].each do |name, _|
+      define_method name do |*args|
+        colored(*args, foreground: name)
+      end
+    end
+
+    BACKGROUNDS[:enable].each do |name, _|
+      define_method "#{name}_background".to_sym do |*args|
+        colored(*args, background: name)
+      end
+    end
+
+    ATTRIBUTES[:enable].each do |name, _|
+      define_method name do |*args|
+        colored(*args, attribute: name)
+      end
+    end
+  end
+end
